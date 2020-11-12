466807868875e5302be2ac3f3142fd576e57a4b7
moksh <moksh@source.one>
2020-11-13 00:11:50 +0530

    reducers made

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const songReducer = () => {
+  return [
+    { title: "No scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2.30" },
+    { title: "I want it that way", duration: "1.45" },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
